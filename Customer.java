663e4f35b1b063e1b1eb046942a61d6403b1da39
kartheeklingam <43851767+kartheeklingam@users.noreply.github.com>
2018-11-05 15:48:49 -0800

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,121 @@
+
+
+public class Customer implements Comparable<Customer> {
+
+	
+	private int id;
+	private String name;
+	private String email;
+	//private LocalDate dob;
+	private String city;
+	
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public Customer(int id, String name, String email, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		//this.dob = dob;
+		this.city = city;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+//	public LocalDate getDob() {
+//		return dob;
+//	}
+
+//	public void setDob(LocalDate dob) {
+//		this.dob = dob;
+//	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+@Override
+public String toString() {
+	
+	return "Customer[Id : "+ id + "Name : " + name + " Email : " + email + " City " + city + "]";
+}
+
+	
+@Override
+public int compareTo(Customer c2) {
+	if(this.name > c2.name) {
+		return 1;
+	}else if {this.name < c2.name){
+		return -1;
+		}
+	return 0;
+}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ Test.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Test{
+
+	public static void main(String[] args){
+	
+		
+		ArrayList<Customer> arrayList = new ArrayList<>();
+		
+		arrayList.add(new Customer(1,"John","john@gmail.com","ABC"));
+		arrayList.add(new Customer(3,"Harry","harry@gmail.com","DEF"));
+		arrayList.add(new Customer(4,"Alicia","alicia@gmail.com","GHI"));
+		arrayList.add(new Customer(2,"Mel","mel@gmail.com","JKL"));
+		arrayList.add(new Customer(5,"Ron","ron@gmail.com","MNO"));
+		
+		
+		
+		}
+		
+		
+	}
+
+
